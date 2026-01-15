5ccade8fba4c23ff3d0c6ecfa874fe941c83246d
Randel <randelserafica1505@gmail.com>
2026-01-16 01:31:09 +0800

    Font and Logo Added

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,30 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Image, StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function Index() {
+  const [fontsLoaded] = useFonts({
+    'Excon-Thin': require('../assets/fonts/Excon-Thin.otf'),
+    'Excon-Light': require('../assets/fonts/Excon-Light.otf'),
+    'Excon-Regular': require('../assets/fonts/Excon-Regular.otf'),
+    'Excon-Medium': require('../assets/fonts/Excon-Medium.otf'),
+    'Excon-Bold': require('../assets/fonts/Excon-Bold.otf'),
+    'Excon-Black': require('../assets/fonts/Excon-Black.otf'),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#7CF205" />
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ headerShown: false }} />
-      <Text style={styles.title}>KARELA</Text>
+
+      <Image source={require('../assets/images/karela_logo.png')} style={styles.logo}/>
+      <Text style={[styles.title, { fontFamily: 'Excon-Bold' }]}>KARELA</Text>
     </View>
   );
 }
@@ -15,6 +34,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#151515',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    resizeMode: 'contain',
+    top: -50
   },
   title: {
     color: '#7CF205',
